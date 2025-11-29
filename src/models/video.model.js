fd48bf494e0676b99df22e52cb554478027ee111
neelpatel1026 <neelpatel8422@gmail.com>
2025-11-29 15:50:46 +0530

    add video and user model, bcrypt and jwt

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            require: true,
+        },
+        thumbnail: {
+            type: String,
+            require: true,
+        },
+        title: {
+            type: String,
+            require: true,
+        },
+        description: {
+            type: String,
+            require: true,
+        },
+        duration: {
+            type: Number,
+            require: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: String,
+            require: true,
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+
+
+export const Video = mongoose.model("Video",videoSchema)
